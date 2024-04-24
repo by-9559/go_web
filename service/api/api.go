8e23e8a9c1a1c9da55ccb4e9d3aaffd8b196f0e9
by <war7ng@qq.com>
2024-04-24 13:37:19 +0800

    增加跨域请求标头

--- service/api/api.go
+++ service/api/api.go
@@ -2,14 +2,33 @@ package api
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+// CORS 中间件
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(http.StatusNoContent)
+            return
+        }
+
+        c.Next()
+    }
+}
+
+
 func GetApi(port int) {
 
 	r := gin.Default()
-
+	r.Use(CORSMiddleware())
 	r.LoadHTMLGlob("templates/*")
 
 	r.GET("/", setip)
